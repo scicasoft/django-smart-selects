257e6ddc46565646f82b92febf842f06cb3301bc
Cheikh Sidya CAMARA <sidya.camara@gmail.com>
2022-01-18 15:34:53 +0000

    fix RemovedInDjango20Warning: Importing from django.core.urlresolvers is deprecated in favor of django.urls.

--- smart_selects/widgets.py
+++ smart_selects/widgets.py
@@ -1,7 +1,7 @@
 import django
 
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.forms.widgets import Select, SelectMultiple
 from django.utils.safestring import mark_safe
 from django.utils.encoding import force_text
